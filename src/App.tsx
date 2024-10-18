e668462f16baa8d31caaabca5e506a84958a56f5
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2024-10-18 22:10:16 +0200

    Add lesson 16

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+// LESSONS
+import Lesson_16 from "lessons/Lesson_16/Lesson_16"
+
 function App() {
-  return <>React Redux App</>
+  return (
+    <>
+      {/* LESSONS */}
+      <Lesson_16 />
+    </>
+  )
 }
 
 export default App
